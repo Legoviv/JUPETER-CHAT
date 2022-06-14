8536a498507a0c700d1adf3056e0cee28d89911b
Legoviv <93427869+Legoviv@users.noreply.github.com>
2022-06-14 12:08:16 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,5 +1,19 @@
-function addUser(){
+var firebaseConfig = {
+    apiKey: "AIzaSyA_fMhM54vDPbchDaXhUhlbwy3SqzF07kY",
+    authDomain: "all-project-5ae38.firebaseapp.com",
+    databaseURL: "https://all-project-5ae38-default-rtdb.firebaseio.com",
+    projectId: "all-project-5ae38",
+    storageBucket: "all-project-5ae38.appspot.com",
+    messagingSenderId: "583572244484",
+    appId: "1:583572244484:web:39041c14276d3af3b20d51"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ function addUser(){
     user_name=document.getElementById("user_name").value;
-    localStorage.setItem("user_name" , user_name);
-    window.location="chat_room.html";
-}
+    firebase.database().ref("/").child(user_name).update({
+        purpose:"adding user"
+    });
+ }
